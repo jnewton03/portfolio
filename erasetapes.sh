5a8a2d3c34adf4ed046dd9784a23b472a5457b53
jnewton03 <31486532+jnewton03@users.noreply.github.com>
2019-02-18 10:07:36 -0600

    dynamically set drive and changer.  Process tapes in parallel (wip)

--- erasetapes.sh
+++ erasetapes.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-set -x
-TAPEDRIVE=/dev/nst0
-SCSITAPE=/dev/sg6
-SCSICHANGER=/dev/sg7
-DRIVE=3
-SLOTS=47
+set -e
+ST_DRIVES=($(lsscsi -g | grep "tape"|awk '{print $6}'))
+SG_DRIVES=($(lsscsi -g | grep "tape"|awk '{print $7}'))
+SCSICHANGER=$(lsscsi -g | grep "mediumx"|awk '{print $7}')
 
-for tape in `seq 1 $SLOTS`;
-do
-    mtx -f $SCSICHANGER load $tape $DRIVE
-    mt -f $TAPEDRIVE rewind
-    sg_format -T 0 $SCSITAPE
-    mtx -f $SCSICHANGER unload $tape $DRIVE
+#DRIVE=3
+SLOTS=($(mtx -f /dev/sg15 status | grep -v CLN | grep -i full | tr ":" " " | awk '{print $3}'))
+BARCODES=($(mtx -f /dev/sg15 status | grep -v CLN | grep -i full | awk -F= '{print $2}'))
+
+
+s=0
+while [ $s -lt ${#SLOTS[@]} ]; do
+  for d in ${SG_DRIVES[@]}; do
+    echo "moving tape ${BARCODES[$s]} to drive $d"
+    #mtx -f $SCSICHANGER load ${SLOTS[$s]} $d
+  let s=s+1
+  done
+  i=0
+  for d in ${ST_DRIVES[@]}; do
+    echo "rewinding tape ${BARCODES[$i]} in drive $d"
+    #(mt -f ${ST_DRIVES[$i]} rewind && sg_format -T 0 ${SG_DRIVES[$i]}) &
+  let i=i+1
+  done
+  for d in ${SG_DRIVES[@]}; do
+    echo "moving tape ${BARCODES[$s]} back to slot"
+    #mtx -f $SCSICHANGER unload $d
+  let s=s+1
+  done
+  wait
 done
-
